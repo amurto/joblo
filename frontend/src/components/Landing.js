95d99b208ad728ed976a777e91de2aac6b2126fe
amurto <amurtobasu@gmail.com>
2020-02-02 07:10:57 +0530

    Footer

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import { SkillContext } from './skill-context';
 
 import { Container, Row, Col } from 'react-bootstrap';
@@ -17,6 +18,7 @@ const Landing = () => {
         <Container className="container-div">
             <Row>
                 <Col sm={true} className="sidediv">
+                    <Link to="/position" style={{ color: "#483D8B" }}>Need a guy for a position?</Link>
                     <h4>Recommended skills</h4>
                     <Filter />
                 </Col>
